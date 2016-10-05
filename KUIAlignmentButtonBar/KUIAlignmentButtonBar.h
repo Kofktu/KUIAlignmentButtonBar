eeb378ec63c745ab14ace18c8cea0f32a228c283
kof.kofktu <kof.kofktu@kakaocorp.com>
2016-10-05 12:56:32 +0900

    프로젝트 기본셋팅

---
+++ KUIAlignmentButtonBar/KUIAlignmentButtonBar.h
@@ -0,0 +1,19 @@
+//
+//  KUIAlignmentButtonBar.h
+//  KUIAlignmentButtonBar
+//
+//  Created by kofktu on 2016. 10. 5..
+//  Copyright © 2016년 Kofktu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KUIAlignmentButtonBar.
+FOUNDATION_EXPORT double KUIAlignmentButtonBarVersionNumber;
+
+//! Project version string for KUIAlignmentButtonBar.
+FOUNDATION_EXPORT const unsigned char KUIAlignmentButtonBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KUIAlignmentButtonBar/PublicHeader.h>
+
+
